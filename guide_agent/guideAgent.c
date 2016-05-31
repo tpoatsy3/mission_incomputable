b8e39a02a3b52d4db80997489a6d6ad57ece1268
Kyra Maxwell <kyra.l.maxwell.18@dartmouth.edu>
2016-05-31 03:00:57 -0400

    Updated guideAgent, worked on memory leaks

--- guide_agent/guideAgent.c
+++ guide_agent/guideAgent.c
@@ -32,7 +32,7 @@
 /******************** local types *************************/
 //Field Agent Struct: Stores a name, status, location, and team 
 typedef struct fieldagent {
-		char *pebbleID;
+		char *name;
 		char *status;
 		double lat;
 		double lng;
@@ -347,6 +347,14 @@ handle_socket(int comm_sock, struct sockaddr_in *themp, hashtable_t *agents,
 					}
 					printf("\n");
 					if (strcmp(messageArray[0], "GAME_STATUS") == 0) {
+						//Delete current hashtables
+						hashtable_delete(agents);
+						hashtable_delete(codeDrops);
+
+						//Create new hashtables with the updated info
+						hashtable_t *agents = hashtable_new(50, data_delete, NULL);
+						hashtable_t *codeDrops = hashtable_new(50, data_delete, NULL);
+						
 						updateGame(messageArray, count, agents, codeDrops, thisGuide);
 						retVal = 1;
 					} else if (strcmp(messageArray[0], "GAME_OVER") == 0) {
@@ -389,23 +397,28 @@ handle_stdin(int comm_sock, struct sockaddr_in *themp, hashtable_t *agents,
 	char *response = readline(stdin);
 	if (response == NULL) 
 		return EOF;
-
+	
+	char *toParse = malloc(strlen(response) + 1);
+	strcpy(toParse, response);
+	
 	if (themp->sin_family != AF_INET) {
 		printf("Error: server is not AF_INET.\n");
 		return 0;
 	} 
 	char *OPCODE;
 	char *respArray[3];
+	printf("RESPONSE %s\n", response);
 	int count = parsingMessages(response, respArray, "|");
 	for(int i = 0; i < count; i++) {
 		free(respArray[i]);
 	}
 	printf("Count in std in %d\n", count);
 	if ( count == 1) 
-		OPCODE = createStatus(response, thisGuide);
-	else if (count == 2) 
-		OPCODE = createHint(response, agents, thisGuide); 
-	else { 
+		OPCODE = createStatus(toParse, thisGuide);
+	else if (count == 2) {
+		printf("RESPONSE %s\n", toParse);
+		OPCODE = createHint(toParse, agents, thisGuide); 
+	} else { 
 		printf("Sorry that message doesn't seem to be formatted correctly");}
 	if (OPCODE != NULL) {
 		printf("%s\n", OPCODE);;
@@ -421,6 +434,7 @@ handle_stdin(int comm_sock, struct sockaddr_in *themp, hashtable_t *agents,
 	return 1;
 }
 
+/************************ internalUpdate *****************/
 static int
 internalUpdate(int comm_sock, int recieve, struct sockaddr_in *themp, 
 	hashtable_t *agents, hashtable_t *codeDrops, guideagent_t *thisGuide)
@@ -505,16 +519,25 @@ createStatus(char *response, guideagent_t *thisGuide)
 char *
 createHint(char *response, hashtable_t *agents, guideagent_t *thisGuide)
 {
-	char* name;
-	char* message;
-	if ((name = strtok(response, "|")) == NULL) return NULL;
-	if ((message = strtok(NULL, "|")) == NULL) return NULL;
+	printf("RESPONSE: %s\n", response);
+	char *pebbleId;
+	char *message;
+	if ((pebbleId = strtok(response, "|")) == NULL){ 
+			printf("PEBBLE ID NULL\n");
+			return NULL;
+	}
+	printf("PebbleID: %s\n", pebbleId);	
+	message = strtok(NULL, "|");
+	if (message == NULL) {
+			printf("MESSAGE NULL\n");
+			return NULL;
+	}
 	if (strlen(message) > 140) {
 		printf("Message is too long\n");
 		return NULL;
 	}
 	fieldagent_t *agent;
-	if ((agent = hashtable_find(agents, name)) == NULL) {
+	if ((agent = hashtable_find(agents, pebbleId)) == NULL) {
 		printf("Sorry that agent doesn't exist");
 		return NULL;
 	} else if (strcmp(agent->team, thisGuide->teamName) != 0) {
@@ -525,7 +548,7 @@ createHint(char *response, hashtable_t *agents, guideagent_t *thisGuide)
 	char *gameId = thisGuide->gameId;
 	char *guideId = thisGuide->id;
 	char *teamName = thisGuide->teamName;
-	char *pebbleId = agent->pebbleID;
+	char *name = thisGuide->name;
 	//create the length for the OPCODE
 	int length = 13 + strlen(gameId) + strlen(guideId) + strlen(teamName) + strlen(name)
 		+ strlen(pebbleId) + strlen(message);
@@ -548,6 +571,7 @@ createHint(char *response, hashtable_t *agents, guideagent_t *thisGuide)
 	strcat(hint, message);
 
 	free(response);
+	printf("%s\n", hint);
 	return hint;
 }
 
@@ -627,11 +651,15 @@ void
 updateGame (char **messageArray, int count, hashtable_t *agents, hashtable_t *codeDrops,
 		guideagent_t *thisGuide) 
 {
-	if (count > 2) {
-		updateAgents(messageArray[2], agents);
+	if (strcmp(thisGuide->gameId,"0") == 0) { // Haven't gotten the gameId yet
+		thisGuide->gameId = messageArray[1]; // Set the gameId
 	}
-	if (count > 3) {
+	if (count == 4) {
+		updateAgents(messageArray[2], agents);
 		updateCodeDrops(messageArray[3], codeDrops);
+	}
+	if (count == 3) { //There are only codeDrops no agents
+		updateCodeDrops(messageArray[2], codeDrops);
 	}
 	hash_iterate(agents, agentPrint, NULL);
 	hash_iterate(codeDrops, codeDropsPrint, NULL);
@@ -654,43 +682,37 @@ updateAgents(char *message, hashtable_t *agents)
 		//There need to be 7 parameters based on the protocol
 		if (paramCount != 7) {
 			printf("Error: only %d parameters per agent\n", paramCount);
-			for (int i = 0; i <= paramCount; i++) {
-				printf(agentUpdate[i]);
-				printf("\n");
-			}
 			return;
 		}
 
 		else {
 			//Look for the agent in the hashtable, (name should be in the third spot)
-			fieldagent_t *current = hashtable_find(agents, agentUpdate[2]);
+			//fieldagent_t *current = hashtable_find(agents, agentUpdate[2]);
 
 			//Create temp variables for the non char* parameters
 			double lat;
 			double lng;
 			double lastcontact;
 			
-			if (current == NULL) {
+			//if (current == NULL) {
 				//fieldagent doesn't exist, so create new data for it
-				current = malloc(sizeof(fieldagent_t));
+			fieldagent_t *current = malloc(sizeof(fieldagent_t));
 
-				//Use given params to fill in the new field agent	
-				sscanf(agentUpdate[4], "%lf", &lat);
-				sscanf(agentUpdate[5], "%lf", &lng);
-				sscanf(agentUpdate[6], "%lf", &lastcontact);
-				current->pebbleID = agentUpdate[0];
-				current->team =agentUpdate[1];
-				current->status = agentUpdate[3];
-				current->lat = lat;
-				current->lng = lng;
-				current->lastcontact = lastcontact;
+			//Use given params to fill in the new field agent	
+			sscanf(agentUpdate[4], "%lf", &lat);
+			sscanf(agentUpdate[5], "%lf", &lng);
+			sscanf(agentUpdate[6], "%lf", &lastcontact);
+			current->team =agentUpdate[1];
+			current->name = agentUpdate[2];
+			current->status = agentUpdate[3];
+			current->lat = lat;
+			current->lng = lng;
+			current->lastcontact = lastcontact;
 				
-				//insert new fieldagent into the hashtable
-				char *name = malloc(strlen(agentUpdate[2])+1);
-				strcpy(name, agentUpdate[2]);
-				hashtable_insert(agents, name, current);
+			//insert new fieldagent into the hashtable
+			hashtable_insert(agents, agentUpdate[0], current);
 			
-			} else {
+			/*} else {
 				//The field agent exists so just update its data
 				sscanf(agentUpdate[4], "%lf", &lat);
 				sscanf(agentUpdate[5], "%lf", &lng);
@@ -706,12 +728,12 @@ updateAgents(char *message, hashtable_t *agents)
 				current->lat = lat;
 				current->lng = lng;
 				current->lastcontact = lastcontact;
-			}
+			} */
 			//Free unused pointers
 			free(agentUpdate[4]);
 			free(agentUpdate[5]);
-			free(agentUpdate[6]);
-		}
+			free(agentUpdate[6]); 
+		} 
 	}
 }
 
@@ -738,31 +760,31 @@ updateCodeDrops(char *message, hashtable_t *codeDrops)
 
 		else {
 			//Look for the code drop in the hashtable, (id should be in the first spot)
-			codedrop_t *current = hashtable_find(codeDrops, dropUpdate[0]);
+			//codedrop_t *current = hashtable_find(codeDrops, dropUpdate[0]);
 
 			//Create temp variables for the non char* parameters
 			double lat;
 			double lng;
 			
-			if (current == NULL) {
+			//if (current == NULL) {
 				//fieldagent doesn't exist, so create new data for it
-				current = malloc(sizeof(codedrop_t));
+			codedrop_t *current = malloc(sizeof(codedrop_t));
 
-				//Use given params to fill in the new field agent	
-				sscanf(dropUpdate[1], "%lf", &lat);
-				sscanf(dropUpdate[2], "%lf", &lng);
-				current->team = dropUpdate[3];
-				if (strcmp(current->team, "NONE") == 0)
-					current->status = "neutralized";
-				else
-					current->status = "active";
-				current->lat = lat;
-				current->lng = lng;
+			//Use given params to fill in the new field agent	
+			sscanf(dropUpdate[1], "%lf", &lat);
+			sscanf(dropUpdate[2], "%lf", &lng);
+			current->team = dropUpdate[3];
+			if (strcmp(current->team, "NONE") == 0)
+				current->status = "active";
+			else
+				current->status = "neutralized";
+			current->lat = lat;
+			current->lng = lng;
 				
-				//insert new fieldagent into the hashtable
-				hashtable_insert(codeDrops, dropUpdate[0], current);
+			//insert new fieldagent into the hashtable
+			hashtable_insert(codeDrops, dropUpdate[0], current);
 			
-			} else {
+			/*} else {
 				//The code drop  exists so just update its data
 				sscanf(dropUpdate[1], "%lf", &lat);
 				sscanf(dropUpdate[2], "%lf", &lng);
@@ -777,10 +799,10 @@ updateCodeDrops(char *message, hashtable_t *codeDrops)
 					current->status = "active";
 				current->lat = lat;
 				current->lng = lng;
-			}
+			} */
 			free(dropUpdate[1]);
 			free(dropUpdate[2]);
-		}
+		} 
 	}
 }
 
@@ -795,21 +817,15 @@ data_delete(void *data)
 void 
 agentPrint(void *key, void *data, void *farg)
 {
-	printf("Agent %s", (char *)key);
-	printf("\n");
 	fieldagent_t *current = data;
-	printf("Pebble ID: ");
-	printf(current->pebbleID);
-	printf(", Team: ");
-	printf(current->team);
-	printf(", Status: ");
-	printf(current->status);
-	printf(", Latitude: ");
-	printf("%lf", current->lat);
-	printf(", Longitude: ");
-	printf("%lf", current->lng);
-	printf(", Seconds since last contact: ");
-	printf("%lf", current->lastcontact);
+	printf("Agent %s", current->name);
+	printf("\n");
+	printf("Pebble ID: %s\n", (char *)key);
+	printf("Team: %s\n",current->team);
+	printf("Status: %s\n", current->status);
+	printf("Latitude: %lf\n", current->lat);
+	printf("Longitude: %lf\n", current->lng);
+	printf("Seconds since last contact: %lf\n\n", current->lastcontact);
 	printf("\n");
 }
 
@@ -819,10 +835,11 @@ codeDropsPrint(void *key, void *data, void *farg)
 	printf("Code Drop %s", (char *)key);
 	printf("\n");
 	codedrop_t *current = data;			
-	printf("%lf",current->lat);
-	printf("%lf",current->lng);
-	printf(current->status);
-	printf(current->team);
+	printf("Latitude: %lf\n",current->lat);
+	printf("Longitude: %lf\n",current->lng);
+	printf("Status: %s\n", current->status);
+	printf("Team: %s\n\n", current->team);
+
 }
 
 int
